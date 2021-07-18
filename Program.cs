077c08da239b5d04ff558dadcc1eba24d5b0cb70
ucpinar <hasanucpinar@gmail.com>
2021-07-18 15:37:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("isminizi girin");
+             string name = Console.ReadLine();
+
+             Console.WriteLine("merhaba " + name );
+            Console.ReadKey();
+            
+        }
+    }
+}
